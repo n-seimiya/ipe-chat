8a45ae571b708bbfd9b7b47b5bdf6cfc593e109b
n-seimiya <naoki.seimiya@ipeinc.jp>
2020-11-09 11:44:42 +0900

    第10章

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,5 +1,47 @@
 class MessagesController < ApplicationController
     def index
         @messages = Message.all
+        @message = Message.new
     end
+
+    def create
+        message = Message.new(create_params)
+        if message.save
+            redirect_to root_path, notice: 'メッセージを送信しました'
+        else
+            redirect_to root_path, notice: 'メッセージの送信に失敗しました'
+        end
+    end
+
+    def edit
+        @message = Message.find(params[:id])
+    end
+
+    def update
+        @message = Message.find(params[:id])
+        if @message.update(update_params)
+            redirect_to root_path, notice: 'メッセージを更新しました'
+        else
+            redirect_to root_path, notixe: 'メッセージの更新に失敗しました'
+        end
+    end
+
+    def destroy
+        @message = Message.find(params[:id])
+        if @message.destroy
+            redirect_to root_path, notice: '削除しました'
+        else
+            redirect_to root_path, notice: 'メッセージの削除に失敗しました'
+        end
+    end
+
+    private
+
+        def create_params
+            params.require(:message).permit(:content).merge(user_id: 1)
+        end
+
+        def update_params
+            params.require(:message).permit(:content)
+        end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
   root 'messages#index'
   get "messages/index", to: "messages#index"
+  post "messages/create", to: "messages#create"
+  get "messages/:id/edit", to: "messages#edit", as: "messages_edit"
+  patch "messages/:id/update", to: "messages#update", as: "messages_update"
+  delete "messages/:id/delete", to: "messages#destroy", as: "messages_delete"
 end
